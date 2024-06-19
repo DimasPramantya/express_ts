da069f9cdb7f5802092a14776a0abe10fdf9b852
DimasPramantya <igkdimas@gmail.com>
2024-06-19 15:01:53 +0700

    REFACTOR empty string in blog router

--- src/routes/blog_router.ts
+++ src/routes/blog_router.ts
@@ -7,15 +7,15 @@ import blog_service from "../service/blog_service";
 
 const router = express.Router();
 
-router.post("", validate_token, has_any_authorities(["ADMIN"]), validateData(blog_schema.create), blog_service.addBlog);
+router.post("/", validate_token, has_any_authorities(["ADMIN"]), validateData(blog_schema.create), blog_service.addBlog);
 
-router.get("", blog_service.getAllBlog);
+router.get("/", blog_service.getAllBlog);
 
 router.get("/:id", blog_service.getBlogById);
 
 router.delete("/:id", validate_token, has_any_authorities(["ADMIN"]), blog_service.deleteBlog);
 
-router.put("", validate_token, has_any_authorities(["ADMIN"]), validateData(blog_schema.update), blog_service.updateBlog);
+router.put("/", validate_token, has_any_authorities(["ADMIN"]), validateData(blog_schema.update), blog_service.updateBlog);
 
 router.get("/users/me", validate_token, has_any_authorities(["ADMIN"]), blog_service.getBlogByAuthor);
 
